20e2e551ccfb63c9a3c2fb54f9829f617d7435c5
Aastik <aastikdude7@gmail.com>
2024-01-12 10:30:46 -0800

    added github link for testing

--- generate.py
+++ generate.py
@@ -8,4 +8,13 @@ def generate_qrcode(text):
     qr = qrcode.QRCode(
         version = 1,
         error_correction = qrcode.constants.ERROR_CORRECTION_L,
+        box_size=10,
+        border=4,
     )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", black_color="white")
+    img.save("qrimg.png")
+
+generate_qrcode("https://github.com/aastik7")
