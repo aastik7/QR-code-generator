108127a750e7149e56f6e899537e8809ceba6947
Aastik <aastikdude7@gmail.com>
2024-01-13 17:16:08 -0800

    changed grid size with version change

--- generate.py
+++ generate.py
@@ -6,7 +6,7 @@ import qrcode
 def generate_qrcode(text):
 
     qr = qrcode.QRCode(
-        version = 1,
+        version = 2,  #Version responsible for grize size
         error_correction = qrcode.constants.ERROR_CORRECT_H,
         box_size=10,
         border=4,
@@ -14,7 +14,7 @@ def generate_qrcode(text):
 
     qr.add_data(text)
     qr.make(fit=True)
-    img = qr.make_image(fill_color="black", black_color="white")
+    img = qr.make_image(fill_color="black", back_color="white")
     img.save("qrimg.png")
 
 url = input("Enter your url: ")
